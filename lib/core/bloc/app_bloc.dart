e7d5ba784c1e1f45c1a604913bfd1e1e8936184c
alirezat66 <alirezataghizadeh66@gmail.com>
2024-06-07 16:20:58 +0200

    implement values and app_bloc

---
+++ lib/core/bloc/app_bloc.dart
@@ -0,0 +1,1 @@
+// this file creates to use single class for importing all blocs in different pages
---
+++ lib/core/constants/values.dart
@@ -0,0 +1,6 @@
+const kVerySmallPadding = 4;
+const kSmallPadding = 8;
+const kMediumPadding = 12;
+const kNormalPadding = 16;
+const kLargePadding = 20;
+const kVeryLargePadding = 24;
